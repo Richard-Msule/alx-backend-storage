60c698e388379bb18884515264fd74f778e07f95
Richard-Msule <msuler@yahoo.com>
2023-07-19 13:03:38 -0700

    web_commit

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,54 +1,84 @@
 #!/usr/bin/env python3
-"""In this tasks, we will implement a get_page function
-(prototype: def get_page(url: str) -> str:). The core of
-the function is very simple. It uses the requests module
-to obtain the HTML content of a particular URL and returns it.
-
-Start in a new file named web.py and do not reuse the code
-written in exercise.py.
-
-Inside get_page track how many times a particular URL was
-accessed in the key "count:{url}" and cache the result with
-an expiration time of 10 seconds.
-
-Tip: Use http://slowwly.robertomurray.co.uk to simulate
-a slow response and test your caching."""
-
-
+"""python module to interact with redis server"""
 import redis
-import requests
+import uuid
+from typing import Union, Optional, Callable
 from functools import wraps
 
-r = redis.Redis()
 
+def call_history(method: Callable) -> Callable:
+    """decorator function to recode parameter & output history"""
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
 
-def url_access_count(method):
-    """decorator for get_page function"""
     @wraps(method)
-    def wrapper(url):
-        """wrapper function"""
-        key = "cached:" + url
-        cached_value = r.get(key)
-        if cached_value:
-            return cached_value.decode("utf-8")
-
-            # Get new content and update cache
-        key_count = "count:" + url
-        html_content = method(url)
-
-        r.incr(key_count)
-        r.set(key, html_content, ex=10)
-        r.expire(key, 10)
-        return html_content
+    def wrapper(self, *args, **kwds):
+        """wrapped function"""
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwds)
+        self._redis.rpush(outputs, str(data))
+        return data
     return wrapper
 
 
-@url_access_count
-def get_page(url: str) -> str:
-    """obtain the HTML content of a particular"""
-    results = requests.get(url)
-    return results.text
+def count_calls(method: Callable) -> Callable:
+    """decorator function"""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """method wrapper"""
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+
+    return wrapper
 
 
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+class Cache:
+    """class to store Redis client information"""
+
+    def __init__(self):
+        """ iniitalize Redis DB """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ create a random key & stores it with data """
+        key: uuid.UUID = uuid.uuid1()
+        self._redis.set(str(key), data)
+        return str(key)
+
+    def get(self, key: str, fn: Optional[Callable]
+            = None) -> Union[str, bytes, int, float]:
+        """convert the data back to the desired format"""
+        data = self._redis.get(key)
+        if fn:
+            data = fn(data)
+        return data
+
+    def get_str(self, data: bytes) -> str:
+        """convert data to string"""
+        return str(data, 'UTF-8')
+
+    def get_int(self, data: bytes) -> int:
+        """convert data to init"""
+        return int.from_bytes(data, "big")
+
+
+def replay(method: Callable):
+    """ display the history of calls for a function """
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+    redis = method.__self__._redis
+    count = redis.get(key).decode("utf-8")
+    print("{} was called {} times:".format(key, count))
+    inputList = redis.lrange(inputs, 0, -1)
+    outputList = redis.lrange(outputs, 0, -1)
+    redis_all = list(zip(inputList, outputList))
+    for a, b in redis_all:
+        attr, data = a.decode("utf-8"), b.decode("utf-8")
+        print("{}(*{}) -> {}".format(key, attr, data))
